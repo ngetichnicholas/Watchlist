650535983dd93810947dcf0d4fcbedae8779b5f4
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-03 18:10:51 +0300

    Add Blueprint

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SEARCH_API_URL ='https://api.themoviedb.org/3/search/movie?api_key={}&query={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
+
