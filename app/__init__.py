d03abd584031d681a04ab3d78c512515f2cb5d41
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-03 18:15:02 +0300

    Add blueprint error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from config import config_options
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-
     app = Flask(__name__)
 
     # Creating the app configurations
@@ -14,6 +13,8 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # Will add the views and forms
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
