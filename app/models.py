525516a5bddfe98968ae050a88e30aa4b5b71645
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-08 11:30:06 +0300

    Add sqlalchemy

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+from . import db
+
 class Movie:
     '''
     Movie class to define Movie Objects
     '''
-
     def __init__(self,id,title,overview,poster,vote_average,vote_count):
         self.id =id
         self.title = title
@@ -10,8 +11,6 @@ class Movie:
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
         self.vote_average = vote_average
         self.vote_count = vote_count
-
-
 
 class Review:
 
@@ -23,10 +22,8 @@ class Review:
         self.imageurl = imageurl
         self.review = review
 
-
     def save_review(self):
         Review.all_reviews.append(self)
-
 
     @classmethod
     def clear_reviews(cls):
@@ -34,11 +31,16 @@ class Review:
 
     @classmethod
     def get_reviews(cls,id):
-
         response = []
-
         for review in cls.all_reviews:
             if review.movie_id == id:
                 response.append(review)
-
         return response
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
