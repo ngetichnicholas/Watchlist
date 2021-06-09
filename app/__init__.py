b9f70d6ea72c530c13c2dd25f8035bb3c7d627d9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-10 02:30:23 +0300

    Profile and email welcome message works

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,19 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -21,6 +28,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
@@ -32,5 +40,8 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
+
+    #configure UploadSet
+    configure_uploads(app,photos)
 
     return app
