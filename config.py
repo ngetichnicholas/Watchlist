e742eea1d3511de3ceb088047d3d84f0573d7394
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-11 12:52:42 +0300

    Update config

--- config.py
+++ config.py
@@ -20,7 +20,10 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
 
 class TestConfig(Config):
